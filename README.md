19fc6cafbdc1025539b930e7b844a834ed372eec
Abhinandh S <ugabhi@proton.me>
2024-11-15 10:08:46 +0530

    new release

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # roxide
 
-`roxide` is a lightweight, high-performance alternative to the traditional `rm` command, designed with an emphasis on speed and data preservation. Unlike standard rm, which permanently deletes files, rid safely moves them to a designated "`trash`" directory. This approach enables efficient and fast file removal without risking unintended data loss.
+`roxide` is a lightweight, high-performance alternative to the traditional `rm` command, designed with an emphasis on speed and data preservation. Unlike standard rm, which permanently deletes files, roxide safely moves them to a designated "`trash`" directory. This approach enables efficient and fast file removal without risking unintended data loss.
 
 ## Getting started
 
@@ -8,8 +8,8 @@
 
 
 ## Features
-- `rid` _filename_ : files will moved to `trash dir`. ie, $HOME/.local/share/Trash/files/
-- `rid --revert` : restors perviously removed file/directory
+- `roxide` _filename_ : files will moved to `trash dir`. ie, $HOME/.local/share/Trash/files/
+- `roxide --revert` : restors perviously removed file/directory
 
 
 ## Installation
@@ -17,55 +17,55 @@
 ## Build
 
 ```bash
-git clone https://github.com/abhi-xyz/rid.git --depth=1 
-cd rid
+git clone https://github.com/abhi-xyz/roxide.git --depth=1 
+cd roxide
 cargo build --release
-cp target/release/rid /usr/local/bin/
+cp target/release/roxide /usr/local/bin/
 ```
 
 
 ### Nixos
 
-Add `rid` as a flake input: In your flake.nix file, add the rid input pointing to its GitHub repository.
+Add `roxide` as a flake input: In your flake.nix file, add the roxide input pointing to its GitHub repository.
 ```nix
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
-    rid = {
-      url = "github:abhi-xyz/rid";
+    roxide = {
+      url = "github:abhi-xyz/roxide";
       inputs.nixpkgs.follows = "nixpkgs"; # Follows the nixpkgs input
     };
   };
 
-  outputs = { self, rid, nixpkgs, ... }@inputs:
+  outputs = { self, roxide, nixpkgs, ... }@inputs:
   {
     # Your NixOS configuration or other settings
   };
 }
 ```
 
-Include rid in NixOS configuration: In the outputs section, set up nixosConfigurations and add rid as a module to enable it within your system configuration.
+Include roxide in NixOS configuration: In the outputs section, set up nixosConfigurations and add roxide as a module to enable it within your system configuration.
 ```nix
 {
-  outputs = { self, rid, nixpkgs, ... }@inputs:
+  outputs = { self, roxide, nixpkgs, ... }@inputs:
   {
     nixosConfigurations."my-nixos-host" = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
       modules = [
         ./hosts/configuration.nix # Main NixOS configuration file
-        rid.nixosModules.rid      # Enable the "rid" module from the flake
+        roxide.nixosModules.roxide      # Enable the "roxide" module from the flake
       ];
     };
   };
 }
 ```
-Activate "rid" in configuration.nix: Within configuration.nix, enable the "rid" program:
+Activate "roxide" in configuration.nix: Within configuration.nix, enable the "roxide" program:
 ```nix
 {
   # Other configurations...
   
-  program.rid.enable = true; # note: its program not programs
+  program.roxide.enable = true; # note: its program not programs
 }
 ```
 Apply Configuration: After updating your configuration, apply it with:
@@ -77,7 +77,7 @@ sudo nixos-rebuild switch --flake .
 - ~~golb pattern isn't working~~
 
 ## Planned Features
-- rid --revert:  Restores files from the trash directory to their original location.
-- rid --gc <TIME_PERIOD>:  Performs garbage collection on trashed files, automatically clearing files older than a specified period.
-- rid -f:  Forces deletion without moving files to the trash directory.
-- rid -s:  Shreds files for secure deletion.
+- roxide --revert:  Restores files from the trash directory to their original location.
+- roxide --gc <TIME_PERIOD>:  Performs garbage collection on trashed files, automatically clearing files older than a specified period.
+- roxide -f:  Forces deletion without moving files to the trash directory.
+- roxide -s:  Shreds files for secure deletion.
--- src/core.rs
+++ src/core.rs
@@ -121,3 +121,150 @@ pub fn remove_files(items: Vec<PathBuf>, recursive: bool, verbose: bool) -> anyh
     }
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use std::fs::remove_dir_all;
+    use std::path::{Path, PathBuf};
+    use std::{fs, path};
+
+    use super::remove_files;
+
+    #[test]
+    fn glob_test() {
+        // FIX: need fix, not an good test
+        let files_for_glob_test = vec![
+            PathBuf::from("glob_test_file_001.txt"),
+            PathBuf::from("glob_test_file_002.txt"),
+            PathBuf::from("glob_test_file_003.txt"),
+            PathBuf::from("glob_test_file_004.txt"),
+            PathBuf::from("glob_test_file_005.txt"),
+            PathBuf::from("glob_test_file_006.txt"),
+            PathBuf::from("glob_test_file_007.txt"),
+            PathBuf::from("glob_test_file_008.txt"),
+        ];
+        for i in &files_for_glob_test {
+            println!("{}", i.display());
+            fs::write(i, "some contents for the files").expect("Cant create files");
+            assert!(fs::exists(i).expect("Can't check existence of file glob_test_file_00x.txt"));
+        }
+        remove_files(files_for_glob_test.clone(), false, true).unwrap();
+        for i in files_for_glob_test {
+            assert!(!fs::exists(i).expect("Can't check existence of file tmp_file.txt"));
+        }
+    }
+
+    #[test]
+    fn single_file_test() {
+        let v0 = PathBuf::from("temp_file_for_single_file_text01.txt");
+        let v1 = PathBuf::from("temp_file_for_single_file_text02.txt");
+        let v2 = PathBuf::from("temp_file_for_single_file_text03.txt");
+        let v3 = PathBuf::from("temp_file_for_single_file_text04.txt");
+        let single_files = vec![v0, v1, v2, v3];
+        for i in &single_files {
+            fs::write(i, "some contents for the files").expect("Cant create files");
+            assert!(fs::exists(i).expect("Can't check existence of file tmp_file.txt"));
+        }
+        remove_files(single_files.clone(), false, true).unwrap();
+        for i in &single_files {
+            assert!(!fs::exists(i).expect("Can't check existence of file tmp_file.txt"));
+        }
+    }
+    #[test]
+    fn single_hidden_file_test() {
+        let v0 = PathBuf::from(".tmp_hidden_file_for_single_file_text01.txt");
+        let v1 = PathBuf::from(".tmp_hidden_file_for_single_file_text02.txt");
+        let v2 = PathBuf::from(".tmp_hidden_file_for_single_file_text03.txt");
+        let v3 = PathBuf::from(".tmp_hidden_file_for_single_file_text04.txt");
+        let single_files = vec![v0, v1, v2, v3];
+        for i in &single_files {
+            fs::write(i, "some contents for the files").expect("Cant create files");
+            assert!(fs::exists(i).expect("Can't check existence of file tmp_file.txt"));
+            pretty_assertions::assert_eq!(1, 1);
+        }
+        remove_files(single_files.clone(), false, true).unwrap();
+        for i in &single_files {
+            assert!(!fs::exists(i).expect("Can't check existence of file tmp_file.txt"));
+        }
+    }
+    #[test]
+    fn remove_file_from_dir_test() {
+        let s = Path::new("some_other").exists();
+        if s {
+            remove_dir_all("some_other").unwrap();
+        }
+        fs::create_dir_all("some_other/dir").unwrap();
+        fs::write("some_other/dir/test.txt", "some contents for testing").unwrap();
+        let v3 = PathBuf::from("some_other/dir/test.txt");
+        let single_files = vec![v3];
+        remove_files(single_files, false, true).expect("Err with my function");
+        assert!(!fs::exists("some_other/dir/test.txt")
+            .expect("Can't check existence of file some/dir/for/testing/tmp_file.txt"));
+        remove_files(
+            vec![path::Path::new("some_other").to_path_buf()],
+            true,
+            true,
+        )
+        .expect("Err with my function");
+        assert!(!fs::exists("some_other")
+            .expect("Can't check existence of file some/dir/for/testing/tmp_file.txt"));
+    }
+    // TEST: done
+    #[test]
+    fn remove_file_from_hidden_dir_test() {
+        let s = Path::new(".some_hidden").exists();
+        if s {
+            remove_dir_all(".some_hidden").expect("Failed to remove existing .some_hidden dir");
+        } else {
+            // creates a hidden dir and a file
+            fs::create_dir_all(".some_hidden/dir").expect("Failed to create .some_hidden/dir for remove_file_from_hidden_dir_test test");
+            fs::write(".some_hidden/dir/test.txt", "some contents for testing").unwrap();
+            let v3 = PathBuf::from(".some_hidden/dir/test.txt");
+            let a_single_file_from_hidden_dir = vec![v3];
+            remove_files(a_single_file_from_hidden_dir, false, true).expect("Err with my function remove_file_from_hidden_dir_test");
+            assert!(!fs::exists(".some_hidden/dir/test.txt").expect("Can't check existence of file some/dir/for/testing/tmp_file.txt"));
+            remove_files(
+                vec![path::Path::new(".some_hidden").to_path_buf()],
+                true,
+                true,
+            )
+                .expect("Err with my function remove_files in remove_file_from_hidden_dir_test");
+            assert!(!fs::exists(".some_hidden")
+                .expect("Can't check existence of file some/dir/for/testing/tmp_file.txt"));
+        }
+    }
+    #[test]
+    fn recursive_remove_test() {
+        let s = Path::new("some").exists();
+        if s {
+            remove_dir_all("some").unwrap();
+        }
+        fs::create_dir_all("some/dir/for/testing").unwrap();
+        let test_dir = vec![PathBuf::from("some")];
+        fs::write("some/test.txt", "some contents for testing").unwrap();
+        remove_files(test_dir, true, true).expect("Err with my function");
+        assert!(!fs::exists("some")
+            .expect("Can't check existence of file some/dir/for/testing/tmp_file.txt"));
+        assert!(!fs::exists("some/dir/for")
+            .expect("Can't check existence of file some/dir/for/testing/tmp_file.txt"));
+        assert!(!fs::exists("some/dir/for/testing")
+            .expect("Can't check existence of file some/dir/for/testing/tmp_file.txt"));
+    }
+    #[test]
+    fn recursive_hidden_dir_remove_test() {
+        let s = Path::new(".some").exists();
+        if s {
+            remove_dir_all(".some").unwrap();
+        }
+        fs::create_dir_all(".some/dir/for/testing").unwrap();
+        let test_dir = vec![PathBuf::from(".some")];
+        fs::write(".some/test.txt", "some contents for testing").unwrap();
+        remove_files(test_dir, true, true).expect("Err with my function");
+        assert!(!fs::exists(".some")
+            .expect("Can't check existence of file some/dir/for/testing/tmp_file.txt"));
+        assert!(!fs::exists(".some/dir/for")
+            .expect("Can't check existence of file some/dir/for/testing/tmp_file.txt"));
+        assert!(!fs::exists(".some/dir/for/testing")
+            .expect("Can't check existence of file some/dir/for/testing/tmp_file.txt"));
+    }
+}
