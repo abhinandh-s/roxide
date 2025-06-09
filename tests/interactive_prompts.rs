5df3b484914f7a2306933ca8fc1836727da1b704
Abhinandh S <abhinandhsuby@proton.me>
2025-06-09 12:24:59 +0530

    Create interactive_prompts.rs

---
+++ tests/interactive_prompts.rs
@@ -0,0 +1,9 @@
+#[test]
+fn test_prompts() {    
+    // TODO: actual test
+    // make the function return vec of requests
+    // then test
+    let result = true;
+    // always true
+    assert!(result)
+}
